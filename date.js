6c5b901e28b1a915c095fbb44e97b2cc1540d216
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-06-25 20:52:52 +0530

    final commit

---
+++ date.js
@@ -0,0 +1,9 @@
+module.exports.getDate = function (){
+    const today = new Date();
+    const options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"        
+    }
+    return  today.toLocaleDateString("en-IN",options);
+}
